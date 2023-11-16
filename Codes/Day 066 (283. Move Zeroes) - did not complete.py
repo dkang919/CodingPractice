c9f51dcc5f884358fee717c4503d1e2bf635e0ff
dsaka2019 <dkang919@gmail.com>
2023-11-16 11:40:21 -0500

    two pointers, in-place algo

---
+++ Codes/Day 066 (283. Move Zeroes) - did not complete.py
@@ -0,0 +1,26 @@
+# solution by _Maximus_
+
+class Solution:
+    def moveZeroes(self, nums: list) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            # wait while we find a non-zero element to
+            # swap with you
+            if nums[slow] != 0:
+                slow += 1
+
+
+
+# solution by areljannC
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        anchor = 0
+        for explorer in range(len(nums)):
+            if nums[explorer] != 0:
+                nums[anchor], nums[explorer] = nums[explorer], nums[anchor]
+                anchor += 1
