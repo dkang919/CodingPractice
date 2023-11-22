7ab3d6dafebede8720eff64304360c8c3b3bcea5
dsaka2019 <dkang919@gmail.com>
2023-11-22 18:46:32 -0500

    math is faster than numpy

---
+++ Codes/Day 072 (342. Power of Four).py
@@ -0,0 +1,6 @@
+from math import log
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <=0:
+            return False
+        return (log(n)/log(4)) == int(log(n)/log(4))
