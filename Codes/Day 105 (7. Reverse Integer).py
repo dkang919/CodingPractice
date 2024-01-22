8d943da24cd66039d79527287e152e0682ee7fc7
dsaka2019 <dkang919@gmail.com>
2024-01-22 17:59:28 -0500

    conditional vs range

---
+++ Codes/Day 105 (7. Reverse Integer).py
@@ -0,0 +1,34 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        s = str(x)
+
+        if "-" in s:
+            s =  "-" + s[::-1][:-1] 
+            if (int(s) < -2147483648) or (int(s) > 2147483647):
+                return 0
+
+        else:
+            s = s[::-1]
+            if (int(s) < -2147483648) or (int(s) > 2147483647):
+                return 0
+
+        return int(s)
+        
+
+------------------------------------------
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        MAX_INT = 2 ** 31 - 1 # 2,147,483,647
+        MIN_INT = -2 ** 31    #-2,147,483,648
+        reverse = 0
+
+        while x != 0:
+            if reverse > MAX_INT / 10 or reverse < MIN_INT / 10:
+                return 0
+            digit = x % 10 if x > 0 else x % -10
+            reverse = reverse * 10 + digit
+            x = math.trunc(x / 10)
+
+        return reverse
